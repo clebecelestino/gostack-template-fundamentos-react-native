b000ca9b3288da948617adc0ef1ba2ea0c271d46
Clebe Celestino <clebecelestino@gmail.com>
2020-08-22 18:20:36 -0300

    first commit

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -25,14 +25,18 @@ const FloatingCart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const sumProducts = products.reduce(
+      (sum, product) => sum + product.quantity * product.price,
+      0,
+    );
 
-    return formatValue(0);
+    return formatValue(sumProducts);
+
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    return products.reduce((sum, product) => sum + product.quantity, 0);
   }, [products]);
 
   return (
